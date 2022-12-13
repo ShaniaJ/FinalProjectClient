a4f75624510dfd3c00fb899223ab22d061873021
ShaniaJ <shania.joseph@aol.com>
2022-12-13 07:31:20 -0500

    add api calls

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,14 @@
 import {store} from "../state/store";
-import {GET_EMPLOYEES, GET_TASKS, UPDATE_TASK} from "../actions";
+import {
+    CREATE_EMPLOYEE,
+    CREATE_TASK,
+    DELETE_EMPLOYEE,
+    DELETE_TASK,
+    GET_EMPLOYEES,
+    GET_TASKS,
+    UPDATE_EMPLOYEE,
+    UPDATE_TASK
+} from "../actions";
 
 const serverURL = 'http://localhost:3001';
 
@@ -25,6 +34,45 @@ const getTasks = () => {
         })
 }
 
+const createTask = (task) => {
+    fetch(`${serverURL}/task/create/`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(task)
+    })
+        .then(response => response.json())
+        .then(response => {
+            store.dispatch({
+                type: CREATE_TASK,
+                task: response.task
+            })
+        })
+}
+
+const createEmployee = (employee) => {
+    fetch(`${serverURL}/employee/create/`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(employee)
+    })
+        .then(response => response.json())
+        .then(response => {
+            store.dispatch({
+                type: CREATE_EMPLOYEE,
+                employee: response.employee,
+                tasks: employee.tasks
+            })
+        })
+}
+
 const updateTask = (task) => {
     fetch(`${serverURL}/task/`, {
         method: 'POST',
@@ -44,4 +92,75 @@ const updateTask = (task) => {
         })
 }
 
-export {getTasks, getEmployees, updateTask};
+const updateEmployee = (employee) => {
+    fetch(`${serverURL}/employee/`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(employee)
+    })
+        .then(response => response.json())
+        .then(response => {
+            store.dispatch({
+                type: UPDATE_EMPLOYEE,
+                employee: response.employee,
+                tasks: employee.tasks
+            })
+        })
+}
+
+const deleteTask = (id) => {
+    fetch(`${serverURL}/task/delete/`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({id: id})
+    })
+        .then(response => response.json())
+        .then(response => {
+            if (response?.rowsDeleted === 1) {
+                store.dispatch({
+                    type: DELETE_TASK,
+                    id: id
+                })
+            }
+        })
+}
+
+const deleteEmployee = (id) => {
+    fetch(`${serverURL}/employee/delete/`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({id: id})
+    })
+        .then(response => response.json())
+        .then(response => {
+            if (response?.rowsDeleted === 1) {
+                store.dispatch({
+                    type: DELETE_EMPLOYEE,
+                    id: id
+                })
+            }
+        })
+}
+
+export {
+    getTasks,
+    getEmployees,
+    createTask,
+    createEmployee,
+    updateTask,
+    updateEmployee,
+    deleteTask,
+    deleteEmployee
+};
